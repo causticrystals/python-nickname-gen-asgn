bc98d4b766e698061ccbcd3eb5182a3690e96b0e
causticrystals <b.papineau-couture@share.epsb.ca>
2023-09-14 13:14:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ while menu:
     #variables
     firstname = "John"
     lastname = "Doe"
-    nickname_list = ['"Crusher"', '"the Scientist"', '"Twinkle-Toes"', '"the Coder"', '"the Jester"', '"the Sloth"', '"Quick-Silver"']
+    nickname_list = ['Crusher', 'the Scientist', 'Twinkle-Toes', 'the Coder', 'the Jester', 'the Sloth', 'Quick-Silver']
 
     #print menu options
     print(f"\nMAIN MENU ({firstname} {lastname})")
@@ -25,21 +25,32 @@ while menu:
     selection = input("\nSelect an option 1-6:")
 
     #action based on input
-    if selection == "1":
+    if selection == "1": #incomplete
         print("\nCHANGE NAME")
-    elif selection == "2":
-        print("\nRANDOM NICKNAME")
+    elif selection == "2": #done
+        print("\nRANDOM NICKNAME") 
         r_name = random.choice(nickname_list)
-        print(f"{firstname} {r_name} {lastname}")
-    elif selection == "3":
+        print(f'{firstname} "{r_name}" {lastname}')
+    elif selection == "3": #incomplete
         print("\nALL NICKNAMES")
         print(*nickname_list, sep ="\n")
-    elif selection == "4":
+    elif selection == "4": #done
         print("\nADD NICKNAME")
         new_nicnam = input("Please enter nickname to add:")
-    elif selection == "5":
+        #test input
+        if new_nicnam in nickname_list:
+            print(f"{new_nicnam} is already in the list!")
+        else:
+            nickname_list.append(new_nicnam)
+    elif selection == "5": #done
         print("\nREMOVE NICKNAME")
         nicnam_gone = input("Please enter nickname to remove:")
-    elif selection == "6":
+        #test input
+        if nicnam_gone in nickname_list:
+            nickname_list.remove(nicnam_gone)
+            print(f'{nicnam_gone} removed from list, bye forever "{nicnam_gone}"!!')
+        else:
+            print(f"{nicnam_gone} was not found in nickname list sorry :-(")
+    elif selection == "6": #done
         print("Bye :-)")
         menu = False
