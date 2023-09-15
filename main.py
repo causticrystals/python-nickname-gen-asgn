9c45bee415e73b9a6ccd62f520def6a81d632048
causticrystals <b.papineau-couture@share.epsb.ca>
2023-09-15 12:58:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Nickname generator
 
-# Import math stuff
-import math
+# Import random stuff
 import random 
+
+#variables
+firstname = "John"
+lastname = "Doe"
+nickname_list = ['Crusher', 'the Scientist', 'Twinkle-Toes', 'the Coder', 'the Jester', 'the Sloth', 'Quick-Silver']
 
 # Menu Loop
 menu = True
 while menu:
-    #variables
-    firstname = "John"
-    lastname = "Doe"
-    nickname_list = ['Crusher', 'the Scientist', 'Twinkle-Toes', 'the Coder', 'the Jester', 'the Sloth', 'Quick-Silver']
 
     #print menu options
     print(f"\nMAIN MENU ({firstname} {lastname})")
@@ -25,24 +25,28 @@ while menu:
     selection = input("\nSelect an option 1-6:")
 
     #action based on input
-    if selection == "1":
+    if selection == "1": #change name
         print("\nCHANGE NAME")
-    elif selection == "2": 
+        firstname = input("Please enter first name: ")
+        lastname = input("Please enter last name: ")
+        print(f"name has been changed to: {firstname} {lastname}")
+    elif selection == "2": #random name
         print("\nRANDOM NICKNAME") 
         r_name = random.choice(nickname_list)
         print(f'{firstname} "{r_name}" {lastname}')
-    elif selection == "3":
-        print("\nALL NICKNAMES")
-        print(*nickname_list, sep ="\n")
-    elif selection == "4": 
+    elif selection == "3": #all names
+        for x in nickname_list:
+            print(f'{firstname} "{x}" {lastname}')
+    elif selection == "4": #add name
         print("\nADD NICKNAME")
-        new_nicnam = input("Please enter nickname to add:")
+        new_nicnam = input("Please enter nickname to add: ")
         #test input
         if new_nicnam in nickname_list:
             print(f"{new_nicnam} is already in the list!")
         else:
             nickname_list.append(new_nicnam)
-    elif selection == "5": 
+            print(f"{new_nicnam} is now in the list :-D!")
+    elif selection == "5": #remove name
         print("\nREMOVE NICKNAME")
         nicnam_gone = input("Please enter nickname to remove:")
         #test input
@@ -51,6 +55,6 @@ while menu:
             print(f'{nicnam_gone} removed from list, bye forever "{nicnam_gone}"!!')
         else:
             print(f"{nicnam_gone} was not found in nickname list sorry :-(")
-    elif selection == "6": 
+    elif selection == "6": #exit loop
         print("Bye :-)")
         menu = False
